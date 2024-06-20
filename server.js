b190da460a165eb8c1a7c07e2983ca5ca5a3df2c
PatrikLuks <120711463+PatrikLuks@users.noreply.github.com>
2024-06-20 17:31:03 +0200

    poznamky server.js

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 const WebSocket = require('ws');
 
+// Vytvori novy WebSocket server na portu 3000
 const server = new WebSocket.Server({ port: 3000 });
 
-let players = [];
+let players = []; // Pole pro ukladani hracu
 
 server.on('connection', (socket) => {
+  // Pokud jsou pripojeni 2 hraci, zavre se nove pripojeni
   if (players.length >= 2) {
     socket.close();
     return;
   }
 
+  // Prida noveho hrace do pole hracu
   players.push(socket);
-  const playerNumber = players.length;
-  socket.send(JSON.stringify({ type: 'init', player: playerNumber }));
+  const playerNumber = players.length; // Cislo hrace
+  socket.send(JSON.stringify({ type: 'init', player: playerNumber })); // Posle zpravu s inicializaci hrace
 
   socket.on('message', (message) => {
     const data = JSON.parse(message);
     if (data.type === 'update') {
+      // Najde druheho hrace
       const opponent = players.find(p => p !== socket);
       if (opponent) {
-        opponent.send(JSON.stringify(data));
+        opponent.send(JSON.stringify(data)); // Posle zpravu protivnikovi
       }
     }
   });
 
   socket.on('close', () => {
+    // Odstrani hrace z pole hracu pri odpojeni
     players = players.filter(p => p !== socket);
   });
 });
 
-console.log('WebSocket server is running on ws://localhost:3000');
+// Vypise informaci o spustenem serveru
+console.log('WebSocket server je spusten na ws://localhost:3000');
